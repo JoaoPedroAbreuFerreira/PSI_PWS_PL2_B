5322e360851e4e73ff8051aa0617306a56b0137d
JPTRON <joaopedroferreira0318@gmail.com>
2022-05-12 16:51:52 +0100

    composer stuff

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9655543b1b1ba7abf77c3e5b72fb4dba811a2a46',
+        'name' => '2211889/pws',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '2211889/pws' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9655543b1b1ba7abf77c3e5b72fb4dba811a2a46',
+            'dev_requirement' => false,
+        ),
+        'php-activerecord/php-activerecord' => array(
+            'pretty_version' => 'v1.2.0',
+            'version' => '1.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-activerecord/php-activerecord',
+            'aliases' => array(),
+            'reference' => 'e6d2dad090617336dee730190be0294c5f132c69',
+            'dev_requirement' => false,
+        ),
+    ),
+);
