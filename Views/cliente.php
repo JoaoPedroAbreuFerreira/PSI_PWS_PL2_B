45eea61d5beb898fae0b9edd44843c0af3153660
JoaoJesus1337 <92525033+JoaoJesus1337@users.noreply.github.com>
2022-05-23 22:20:26 +0100

    css update

--- Views/cliente.php
+++ Views/cliente.php
@@ -1,3 +1,9 @@
-Bem vindo <?= $_SESSION["username"]?> [Cliente]
+<link rel="stylesheet" type="text/css" href="public/css/A.css">	
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+<label class="ok">BEM VINDO <?= $_SESSION["username"]?></label><label class="ok3">[CLIENTE]</label>
 <br>
-<a href='index.php?r=auth/logout'>Log Out</a>
+<div align="center">
+<a href='index.php?r=auth/logout' class="btn btn-primary" role="button"id="coids">Log Out</a>
+</div>
