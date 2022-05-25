092fcdabb7be1cd234394672db88bece6bfa23dd
JPTRON <joaopedroferreira0318@gmail.com>
2022-05-25 23:25:42 +0100

    fix cone de gelado

--- Views/imprimirFatura.php
+++ Views/imprimirFatura.php
@@ -12,32 +12,32 @@
 </div>
 <div>
     <table class="table">
-    <thead>
-        <th>Referencia</th>
-        <th>Descrição</th>
-        <th>Quantidade</th>
-        <th>Valor Uni.</th> 
-        <th>IVA %</th>
-        <th>IVA</th>
-        <th>Subtotal</th>
-    </thead>
-    <tbody>
-        <?php foreach ($linhas as $linha) { ?>
-            <tr>
-                <td><?=Produto::find_by_id($linha->produto_id)->referencia?></td>
-                <td><?=Produto::find_by_id($linha->produto_id)->descricao?></td>
-                <td><?=$linha->quantidade?></td>
-                <td><?=Produto::find_by_id($linha->produto_id)->preco?>€</td>
-                <td><?=Iva::find_by_id(Produto::find_by_id($linha->produto_id)->iva_id)->percentagem?>%</td>
-                <td><?=$linha->valoriva?>€</td>
-                <td><?=$linha->valor?>€</td>
-            </tr>
-        <?php } ?>
-    </tbody>
-    <tr>
+        <thead>
+            <th>Referencia</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Uni.</th> 
+            <th>IVA %</th>
+            <th>IVA</th>
+            <th>Subtotal</th>
+        </thead>
+        <tbody>
+            <?php foreach ($linhas as $linha) { ?>
+                <tr>
+                    <td><?=Produto::find_by_id($linha->produto_id)->referencia?></td>
+                    <td><?=Produto::find_by_id($linha->produto_id)->descricao?></td>
+                    <td><?=$linha->quantidade?></td>
+                    <td><?=Produto::find_by_id($linha->produto_id)->preco?>€</td>
+                    <td><?=Iva::find_by_id(Produto::find_by_id($linha->produto_id)->iva_id)->percentagem?>%</td>
+                    <td><?=$linha->valoriva?>€</td>
+                    <td><?=$linha->valor?>€</td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    <tfooter>
         <td><b>Total</b></td>
         <td><?=$fatura->valortotal?>€</td>
-    </tr>>
+    </tfooter>
 </table>
 </div>
 <hr>
